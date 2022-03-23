f72cca617c948aca9af8b33d205229ed366f42be
Sha <99729188+Sha-Camiso@users.noreply.github.com>
2022-03-23 21:11:33 +0900

    Update Freecodecamp.js

--- Freecodecamp.js
+++ Freecodecamp.js
@@ -1,3 +1,5 @@
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 const myStr = "This is the start. " + "This is the end."; 
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
