b1abcd7061bc472fadaf7145ee3e81a8e058048f
Sha <99729188+Sha-Camiso@users.noreply.github.com>
2022-03-24 14:02:50 +0900

    Update Freecodecamp.js

--- Freecodecamp.js
+++ Freecodecamp.js
@@ -3,3 +3,11 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 const myStr = "This is the start. " + "This is the end."; 
 var myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Only change this line;
+// Only change code above this line
